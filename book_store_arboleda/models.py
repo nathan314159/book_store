84a541ab97f14a51e40d3db058b58c37ea59cacb
Nathan Arboleda <arboledanathan1@gmail.com>
2023-06-15 13:22:54 -0500

    commit

--- book_store_arboleda/models.py
+++ book_store_arboleda/models.py
@@ -9,38 +9,15 @@ def default_price():
 
 class Book(models.Model):
     title=models.CharField(max_length=200,null=True)
-    author=models.CharField(max_length=200,null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, default=default_price)
-    total_copies = models.IntegerField(default=0)
-    copies_in_stock = models.IntegerField(default=0)
-    def __str__(self):
-        return str(self.title)
+    stock = models.ForeignKey('Stock', on_delete=models.CASCADE, null = True)
+    authors = models.ManyToManyField('Author')  
     
-    def add_to_stock(self, num_copies):
+    def __str__(self):
+        author_names = ", ".join(author.name for author in self.authors.all())
+        return f'{self.title} (Authors: {author_names})'
 
-        self.copies_in_stock += num_copies
-        self.total_copies += num_copies
-        self.save()
-
-    def remove_from_stock(self, num_copies):
-
-        if self.copies_in_stock >= num_copies:
-            self.copies_in_stock -= num_copies
-            self.save()
-        else:
-            raise ValueError("Cannot remove more copies than in stock.")
-
-    def decrease_stock(self, num_copies):
-        if self.copies_in_stock >= num_copies:
-            self.copies_in_stock -= num_copies
-            self.save()
-        else:
-            raise ValueError("Not enough copies in stock.")   
-        
-    def increase_stock(self, num_copies):
-        self.copies_in_stock += num_copies
-        self.total_copies += num_copies
-        self.save()     
+   
 
  
 class Cart(models.Model): 
@@ -60,7 +37,11 @@ class Cart(models.Model):
             total += book.price
         self.total_amount = total
         self.save()
-
+        
+    def save(self, *args, **kwargs):
+        self.book_count = self.books.count()  # Update the book count
+        super().save(*args, **kwargs)
+    
     def __str__(self):
         book_names = ', '.join([str(book) for book in self.books.all()])
         return f"Books: {book_names}"
@@ -77,13 +58,7 @@ class Payment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     date = models.DateField()
     payment_method = models.CharField(max_length=50)
-    
-    # def save(self, *args, **kwargs):
-    #     super().save(*args, **kwargs)
-    #     cart = Cart.objects.get(user=self.user)
-    #     if cart.book_count > 0:
-    #         cart.empty_cart()
-    #         cart.update_total_amount()
+
 
 class Invoice(models.Model):
     payment = models.ForeignKey(Payment, on_delete=models.CASCADE)
@@ -101,3 +76,36 @@ class Invoice_detail(models.Model):
         return f"Invoice detail {self.invoice.id}"
 
 
+class Author(models.Model):
+    name = models.CharField(max_length=20,null=True)
+
+    
+
+class Stock(models.Model):
+    total_copies = models.IntegerField(default=0)
+    copies_in_stock = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return f'{self.total_copies}'
+
+    def decrease_stock(self, num_copies):
+        if self.copies_in_stock >= num_copies:
+            self.copies_in_stock -= num_copies
+            self.save()
+        else:
+            raise ValueError("Not enough copies in stock.")   
+        
+    def increase_stock(self, num_copies):
+        self.copies_in_stock += num_copies
+        self.total_copies += num_copies
+        self.save()
+        
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=20)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
